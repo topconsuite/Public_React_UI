7d532f1cd4cf6ef72d7caeda8cc8440b1c5c7de4
Adenilson Silva <adenilson@telluria.com.br>
2021-10-22 15:49:04 -0300

    fix(tests): bug fix tests

--- src/atoms/Toast/index.test.tsx
+++ src/atoms/Toast/index.test.tsx
@@ -1,9 +1,10 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Toast from "./index";
+import { TelluriaProvider } from "../..";
 
-describe("avatar component", () => {
+describe("Toast component", () => {
   it("should render the component", () => {
-    render(<Toast message={{ id: "1", title: "Title", description: "description" }} style={{}} />);
+    render(<Toast message={{ id: "1", title: "Title", description: "description" }} style={{}} />, { wrapper: TelluriaProvider });
   });
 });
