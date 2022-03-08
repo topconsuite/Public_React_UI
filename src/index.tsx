d7eecafb7076a55574f3ac891bbe0fe29a562830
Adenilson A. da Silva <adenilson@topsys.com>
2022-03-08 18:16:09 -0300

    feat(all): add tooltip component and barrel imports on components

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,16 @@
 import TelluriaProvider from "./hooks";
 import { useToast } from "./hooks/useToast/useToast";
 
-import TextField from "./atoms/TextField";
-import FloatActionButton from "./atoms/FloatActionButton";
-import SpeedDialMenu from "./atoms/SpeedDialMenu";
-import SidebarMenu, { SidebarMenuProps } from "./atoms/SidebarMenu";
-import SidebarDrawerMenu, { SidebarDrawerMenuProps } from "./atoms/SidebarDrawerMenu";
+export { TelluriaProvider, useToast };
 
-import Sidebar from "./molecules/Sidebar";
-import SpeedDial from "./molecules/SpeedDial";
-import SidebarDrawer from "./molecules/SidebarDrawer";
+export { Sidebar, SpeedDial, SidebarDrawer } from "./molecules";
 
 export {
-  TelluriaProvider,
-  useToast,
   TextField,
   FloatActionButton,
   SpeedDialMenu,
-  Sidebar,
-  SpeedDial,
-  SidebarDrawer,
   SidebarMenu,
   SidebarMenuProps,
-  SidebarDrawerMenu,
-  SidebarDrawerMenuProps
-};
+  SidebarDrawerMenuProps,
+  SidebarDrawerMenu
+} from "./atoms";
